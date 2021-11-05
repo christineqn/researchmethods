0cc3d348b2f253a52cb983910d2b7b94db079cda
christineqnguyen <93330159+christineqn@users.noreply.github.com>
2021-11-05 17:31:33 -0400

    updated hw1 for hw2

--- HW1/hw1.R
+++ HW1/hw1.R
@@ -1,4 +1,4 @@
-setwd('C:/Users/c1/OneDrive/Desktop/Fall 2021/Research methods II/model assignment')
+setwd('C:/Users/c1/OneDrive/Desktop/Fall 2021/Research methods II/researchmethods/HW1')
 
 library('stargazer')
 library('dplyr')
@@ -6,26 +6,34 @@ library('dplyr')
 # Read in data:
 data <- read.csv('assignment1-research-methods.csv')
 
-# Label your variables
-data <- rename(data,
-               CandidateID=candidateid,
-               CalledBack=calledback,
-               RecruiterIsWhite=recruiteriswhite,
-               RecruiterIsMale=recruiterismale)
-
 # Run regression: 
 # effect of having an elite college on whether the fictitious candidate's job application was called back
-regression <- lm(CalledBack ~ EliteSchoolCandidate, data)
-regression2 <- lm(CalledBack ~ EliteSchoolCandidate*MaleCandidate, data)
-regression3 <- lm(CalledBack ~ EliteSchoolCandidate*BigCompanyCandidate, data)
-regression4 <- lm(CalledBack ~ EliteSchoolCandidate*RecruiterIsWhite, data)
-regression5 <- lm(CalledBack ~ EliteSchoolCandidate + 
-                    RecruiterIsWhite + RecruiterIsMale + 
+regression <- lm(calledback ~ EliteSchoolCandidate, data)
+regression2 <- lm(calledback ~ EliteSchoolCandidate*MaleCandidate, data)
+regression3 <- lm(calledback ~ EliteSchoolCandidate*BigCompanyCandidate, data)
+regression4 <- lm(calledback ~ EliteSchoolCandidate*recruiteriswhite, data)
+regression5 <- lm(calledback ~ EliteSchoolCandidate + 
+                    recruiteriswhite + recruiterismale + 
                     MaleCandidate + BigCompanyCandidate, data)
+regression6 <- lm(calledback ~ EliteSchoolCandidate + recruiteriswhite +
+                    recruiterismale + BigCompanyCandidate, data)
 
 # Output:
 stargazer(regression, type='text', dep.var.caption='', omit.stat=c('LL','ser','f'), digit.separator='', digits=2, initial.zero=F, intercept.bottom=F, no.space=T, notes='Standard errors in parentheses.')
 stargazer(regression, regression2, regression3, regression4, regression5,
-          type='html', dep.var.caption='', omit.stat=c('LL','ser','f'), digit.separator='', digits=2, initial.zero=F, intercept.bottom=F, no.space=T, notes='Standard errors in parentheses.',
+          type='text', dep.var.caption='', dep.var.labels=c('Called Back'), omit.stat=c('LL','ser','f'), digit.separator='', digits=2, initial.zero=F, intercept.bottom=F, no.space=T, notes='Standard errors in parentheses.',
+          covariate.labels=c('Constant','Elite School Candidate',
+                             'Male Candidate','Elite School Candidate x Male Candidate',
+                             'Big Company Candidate','Elite School Candidate x Big Company Candidate',
+                             'Recruiter is White','Elite School Candidate x Recruiter is White',
+                             'Recruiter is Male'),
+          out='hw1-table.doc')
+# removing control for whether the candidate was male or female 
+stargazer(regression, regression3, regression4, regression6,
+          type='html', dep.var.caption='', dep.var.labels=c('Called Back'), omit.stat=c('LL','ser','f'), digit.separator='', digits=2, initial.zero=F, intercept.bottom=F, no.space=T, notes='Standard errors in parentheses.',
+          covariate.labels=c('Constant','Elite School Candidate',
+                             'Big Company Candidate','Elite School Candidate x Big Company Candidate',
+                             'Recruiter is White','Elite School Candidate x Recruiter is White',
+                             'Recruiter is Male'),
           out='hw1-table.doc')
 # * p<0.1, ** p<0.05, *** p<0.01
